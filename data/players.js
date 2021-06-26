d89ba2d9489395c8632b0484b4ad6e08de731ac7
Kyle Coberly <kyle.coberly@gmail.com>
2021-06-26 11:13:27 -0600

    Initial commit

---
+++ data/players.js
@@ -0,0 +1,26 @@
+module.exports = [{
+    id: 1,
+    fullName: "Kevin Corsican",
+    title: "Captain",
+    numberOfDogs: 0
+},{
+    id: 2,
+    fullName: "Don Fritz",
+    title: "Wingman",
+    numberOfDogs: 0
+},{
+    id: 7,
+    fullName: "Kelly Stuart",
+    title: "Helm",
+    numberOfDogs: 0
+},{
+    id: 8,
+    fullName: "Patrick Octavian",
+    title: "Striker",
+    numberOfDogs: 1
+},{
+    id: 10,
+    fullName: "Michael Wallingford",
+    title: "Steersman",
+    numberOfDogs: 2
+}];
